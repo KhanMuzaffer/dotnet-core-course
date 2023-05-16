05191a8cfdc59a6bd477a47a492d906c5ec9e8db
KhanMuzaffer <khanmuzaffer18913@gmail.com>
2023-05-16 11:07:29 +0530

    Task GetById, Save functionality done

---
+++ session20_15-May-2023/TaskTracker/TaskTracker/Controllers/TaskController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TaskTracker.IServices;
+
+namespace TaskTracker.Controllers
+{
+    public class TaskController : Controller
+    {
+        private readonly ITaskRepository _repository;
+
+        public TaskController(ITaskRepository  repository)
+        {
+            this._repository = repository;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
